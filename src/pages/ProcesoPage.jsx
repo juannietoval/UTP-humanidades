4256a11b5b39f52269f4f98605be13e4bdee3e56
juannietoval <juan.nieto2@utp.edu.co>
2026-07-29 19:20:32 -0500

    feat: create ProcesoPage component to display syllabus units grid

--- src/pages/ProcesoPage.jsx
+++ src/pages/ProcesoPage.jsx
@@ -31,25 +31,6 @@ export default function ProcesoPage() {
           ))}
         </div>
 
-        <div className="grid" style={{ marginBottom: 'calc(var(--base-unit) * 8)' }}>
-          <div className="card" style={{ gridColumn: 'span 4' }}>
-            <div style={{ border: '1px solid var(--primary)', padding: 'calc(var(--base-unit)*4)', aspectRatio: '3/4', display: 'flex', alignItems: 'center', justifyContent: 'center', background: 'var(--tertiary)', marginBottom: 'calc(var(--base-unit) * 2)' }}>
-              <h1 style={{ margin: 0 }}>2026-1</h1>
-            </div>
-            <span className="label">Semestre</span>
-            <h3 className="card-title">2026-1</h3>
-            <p className="card-text"></p>
-          </div>
-
-          <div className="card" style={{ gridColumn: 'span 4' }}>
-            <div style={{ border: '1px solid var(--primary)', padding: 'calc(var(--base-unit)*4)', aspectRatio: '3/4', display: 'flex', alignItems: 'center', justifyContent: 'center', background: 'var(--tertiary)', marginBottom: 'calc(var(--base-unit) * 2)' }}>
-              <h1 style={{ margin: 0 }}>2026-2</h1>
-            </div>
-            <span className="label">Semestre</span>
-            <h3 className="card-title">2026-2</h3>
-            <p className="card-text"></p>
-          </div>
-        </div>
       </div>
     </section>
   );
